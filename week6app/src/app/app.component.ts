45c472a6d1d348c520aafca6ceab9c14e260fb2c
BrentPresley <brent.presley@mstc.edu>
2019-02-24 21:30:39 -0600

    initial application -includes services, promises, error handling,animatn

---
+++ week6app/src/app/app.component.ts
@@ -0,0 +1,242 @@
+import { Component, OnInit } from '@angular/core';
+import { QuizService } from './quiz.service';
+
+import {
+  trigger
+  , style
+  , animate
+  , transition
+  , keyframes
+} from '@angular/animations';
+import { LifecycleHooks } from '@angular/compiler/src/lifecycle_reflector';
+
+interface QuizDisplay {
+
+  name: string;
+  originalName: string;
+
+  numberQuestions: number;
+
+  questions: QuestionDisplay[];
+  naiveQuestionsChecksum: string;
+}
+
+interface QuestionDisplay {
+  name: string;
+}
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('detailsFromLeft', [
+      transition('leftPosition => finalPosition', [
+        animate('300ms', keyframes([
+          style({ left: '-30px', offset: 0.0 }),
+          style({ left: '-20px', offset: 0.25 }),
+          style({ left: '-10px', offset: 0.5 }),
+          style({ left: '-5px', offset: 0.75 }),
+          style({ left: '0px', offset: 1.0 })
+        ]))
+      ]),
+    ]),
+    trigger('pulseSaveCancelButtons', [
+      transition('nothingToSave => somethingToSave', [
+        animate('400ms', keyframes([
+          style({ transform: 'scale(1.0)', 'transform-origin': 'top left', offset: 0.0 }),
+          style({ transform: 'scale(1.2)', 'transform-origin': 'top left', offset: 0.5 }),
+          style({ transform: 'scale(1.0)', 'transform-origin': 'top left', offset: 1.0 })
+        ]))
+      ])
+    ])
+  ]
+})
+export class AppComponent implements OnInit {
+
+  constructor (private quizSvc: QuizService) {
+  }
+
+
+  // numberOfChangedQuizzes = 2;
+
+  // TS readonly property...
+  get numberOfChangedQuizzes() {
+    const changedQuizzes = this.quizzes.filter(x =>
+      x.name !== x.originalName
+      || x.originalName === 'New Untitled Quiz'
+      || x.naiveQuestionsChecksum !== x.questions.map(y => y.name).join('~')
+    );
+    return changedQuizzes.length;
+  }
+
+  quizzes: QuizDisplay[] = [];
+  wasErrorLoadingQuizzes = false;
+
+  selectedQuiz = undefined;
+
+  detailsAnimationState = 'leftPosition';
+
+  title = 'quiz-editor';
+
+  dumb = 'foo123';
+
+  titleColorDanger = this.dumb === 'foo' ? true : false;
+  titleBackgroundColorDanger = false;
+
+  headingTwoBackgroundColor = this.dumb === 'foo' ? 'Red' : 'Blue';
+
+  imageWidth = 100;
+
+  ngOnInit() {
+    // console.log(this.quizSvc.getQuizzes());
+    this.loadQuizzes();
+  }
+
+  private loadQuizzes() {
+
+     (data) => {
+         console.log(data);
+         this.quizzes = data
+       }
+
+    //this.quizSvc.getQuizzes().subscribe(
+    //  data => this.quizzes = (<QuizDisplay[]>data).map(x => ({
+    //    ...x,
+    //    originalName: x.name,
+    //    naiveQuestionsChecksum: x.questions.map(y => y.name).join('~')
+    //  })), error => this.wasErrorLoadingQuizzes = true);
+
+
+  }
+
+  reloadQuizzes() {
+    //console.log('Foo');
+    this.selectedQuiz = undefined;
+    this.loadQuizzes();
+  }
+
+  selectQuiz(q) {
+    // console.log(q);
+    this.selectedQuiz = q;
+    this.detailsAnimationState = 'finalPosition';
+  }
+
+  saveQuizzes() {
+    //console.log('saveQuizzes()');
+    const changedQuizzes = this.quizzes
+    .filter(x =>
+      x.originalName !== 'New Untitled Quiz'
+      && (x.name !== x.originalName
+      || x.naiveQuestionsChecksum !== x.questions.map(y => y.name).join('~'))
+    );
+
+    this.quizSvc.saveQuizzes(changedQuizzes).subscribe(
+      data => console.log(data)
+      , error => console.log(error)
+    );
+
+  }
+
+  addNewQuiz() {
+    const q = {
+      name: 'New Untitled Quiz'
+      , originalName: 'New Untitled Quiz'
+      , numberQuestions: 0
+      , questions: []
+      , naiveQuestionsChecksum: ''
+    };
+
+    this.quizzes = [...this.quizzes, q];
+    this.selectQuiz(q);
+  }
+
+  addNewQuestion(selectedQuiz) {
+    selectedQuiz.questions = [...selectedQuiz.questions, {'name': 'New untitled question'}];
+    selectedQuiz.numberQuestions = selectedQuiz.questions.length;
+
+    console.log(this.numberOfChangedQuizzes);
+    // this.numberOfChangedQuizzes = 75;
+
+    console.log(this.selectedQuiz.naiveQuestionsChecksum);
+  }
+
+  removeQuestion(selectedQuiz, selectedQuestion) {
+    selectedQuiz.questions = selectedQuiz.questions.filter(n => n !== selectedQuestion);
+    selectedQuiz.numberQuestions = selectedQuiz.questions.length;
+  }
+
+  detailsFromLeftAnimationComplete() {
+    this.detailsAnimationState = 'leftPosition';
+  }
+
+  // Learning promises functions below...
+
+
+  learningPromises() {
+    console.log('learningPromises()');
+
+    const x = this.quizSvc.getNumberOfQuizzes(true);
+    console.log(x);
+
+    x.then(
+      n => {
+        console.log(n);
+
+        const y = this.quizSvc.getNumberOfQuizzes(false);
+        console.log(y);
+
+        y.then(
+          n2 => console.log(n2)
+        ).catch(
+          e => console.log(e)
+        );
+      }
+
+    ).catch(
+      e => console.log(e)
+    );
+
+
+  }
+
+  async learningPromisesWithAsyncAwait() {
+    console.log('learningPromisesWithAsyncAwait()');
+
+    try {
+      const x = await this.quizSvc.getNumberOfQuizzes(true);
+      console.log(x);
+
+      const y = await this.quizSvc.getNumberOfQuizzes(false);
+      console.log(y);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async learningPromisesWithAwaitAll() {
+    console.log('learningPromisesWithAsyncAwait()');
+
+    try {
+      const x = this.quizSvc.getNumberOfQuizzes(true);
+      console.log(x);
+
+      const y = this.quizSvc.getNumberOfQuizzes(true);
+      console.log(y);
+
+      // let results = await Promise.race([x, y]);
+      const results = await Promise.all([x, y]);
+      console.log(results);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  // imageWidth = '100px';
+
+  increaseImageWidth = () => this.imageWidth *= 1.5;
+
+
+
+}
+
